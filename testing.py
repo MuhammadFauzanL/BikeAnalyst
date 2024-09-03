3f8e353084b4fb4d30720cc448bddeda98b836bd
Fauzan <141022549+MuhammadFauzanL@users.noreply.github.com>
2024-09-03 20:16:19 +0700

    Update testing.py

--- testing.py
+++ testing.py
@@ -6,6 +6,14 @@ import seaborn as sns
 hour_df = pd.read_csv("https://raw.githubusercontent.com/MuhammadFauzanL/BikeAnalyst/main/hour_dataa.csv")
 day_df = pd.read_csv("https://raw.githubusercontent.com/MuhammadFauzanL/BikeAnalyst/main/day_dataFrame.csv")
 
+
+description = {
+    1: 'Cerah',
+    2: 'Mendung',
+    3: 'Gerimis',
+    4: 'Hujan Deras'
+}
+hour_df['weather_situation'] = hour_df['weather_situation'].map(description)
 hour_df['hour'] = hour_df['hour'].apply(lambda x: f"{x:02d}:00")
 
 total_by_weather_hour = hour_df.groupby(['weather_situation', 'hour'])['count'].sum().reset_index()
@@ -15,18 +23,19 @@ total_by_hour = hour_df.groupby('hour')['count'].sum().reset_index()
 day_df['dteday'] = pd.to_datetime(day_df['dteday'])
 day_df['day_type'] = day_df['workingday'].apply(lambda x: 'Hari Kerja' if x == 1 else 'Akhir Pekan')
 total_registered = day_df.groupby('day_type')['registered'].sum().reset_index()
-
 st.title('Analisis Penyewaan Sepeda Dicoding')
 
 st.subheader('Total Penyewaan Sepeda Berdasarkan Cuaca dan Jam')
-fig, ax = plt.subplots(figsize=(15, 6))
+fig, ax = plt.subplots(figsize=(17, 6))
 sns.lineplot(data=total_by_weather_hour, x='hour', y='count', hue='weather_situation', marker='o', ax=ax)
 ax.set_title('Total Penyewaan Sepeda Berdasarkan Cuaca dan Jam')
 ax.set_xlabel('Jam')
 ax.set_ylabel('Jumlah Penyewaan')
 st.pyplot(fig)
 
+
 st.subheader('Total Penyewaan Sepeda Berdasarkan Cuaca')
+
 fig, ax = plt.subplots(figsize=(8, 5))
 sns.barplot(data=total_by_weather, x='weather_situation', y='count', ax=ax)
 ax.set_title('Total Penyewaan Sepeda Berdasarkan Cuaca')
@@ -35,7 +44,8 @@ ax.set_ylabel('Jumlah Penyewaan')
 st.pyplot(fig)
 
 st.subheader('Total Penyewaan Sepeda Berdasarkan Jam')
-fig, ax = plt.subplots(figsize=(12, 6))
+
+fig, ax = plt.subplots(figsize=(17, 6))
 sns.barplot(data=total_by_hour, x='hour', y='count', ax=ax)
 ax.set_title('Total Penyewaan Sepeda Berdasarkan Jam')
 ax.set_xlabel('Jam')
@@ -52,3 +62,6 @@ ax.set_title('Total Pendaftaran Sepeda Berdasarkan Hari Kerja dan Akhir Pekan')
 ax.set_xlabel('Tipe Hari')
 ax.set_ylabel('Total Pendaftaran Sepeda')
 st.pyplot(fig)
+
+
+
